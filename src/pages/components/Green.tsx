e1d0fc900de15afb512270f2ca7cb37e0623b1c8
KathrynBrusewitz <kathryn.brusewitz@gmail.com>
2022-11-12 12:55:06 -0500

    Add build branch

--- src/pages/components/Green.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const Green = ({ children }: PropsWithChildren) => {
-  return <span style={{ color: "green" }}>{children}</span>;
-};
-
-export default Green;
--- src/wordle/constants/strings.ts
+++
@@ -1,4 +0,0 @@
-export const NOT_ENOUGH_LETTERS_MESSAGE = "Not enough letters";
-export const WORD_NOT_FOUND_MESSAGE = "Word not found";
-export const ENTER_TEXT = "Enter";
-export const DELETE_TEXT = "Delete";
